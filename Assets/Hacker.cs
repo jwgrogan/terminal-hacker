02fdbdf2e4a8be30bc7876a287ff9c7a179efb57
Jacob Grogan <jagrogan@microsoft.com>
2020-04-02 10:34:57 -0700

    initial commit

---
+++ Assets/Hacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("sup fool");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
